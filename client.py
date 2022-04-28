2bc808fdb553e68c762de6dde62b754d1aec60d4
Rahul Makhija <rmrahulmakhija74@gmail.com>
2022-04-28 22:47:13 +0530

    integrate with  kafka and mongo

--- client.py
+++ client.py
@@ -1,10 +1,32 @@
 import json
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 from pyspark.sql.session import SparkSession
 from pyspark.streaming import StreamingContext
 import pyspark.sql.types as tp
 # import pyspark.sql.Row as Row
 from pyspark.sql import Row
+
+
+from pyspark.streaming.kafka import KafkaUtils
+from kafka import KafkaProducer
+from collections import Counter
+
+producer = KafkaProducer(bootstrap_servers='localhost:9009', value_serializer=str.encode, key_serializer=str.encode)
+
+
+
+conf = SparkConf()
+#set name for our app
+conf.setAppName("ConnectingDotsSparkKafkaStreaming")
+#The master URL to connect
+conf.setMaster('localhost:7077')
+sc = None
+try:
+        sc.stop()
+        sc = SparkContext(conf=conf)
+except:
+        sc = SparkContext(conf=conf)
+
 sc = SparkContext(appName="PySparkShell")
 spark = SparkSession(sc)
 
@@ -15,6 +37,7 @@ def process(rdd):
             .getOrCreate()
 
     rowRdd = rdd.map(lambda x: Row(word=x))
+    
     if not rowRdd.isEmpty():
         wordsDataFrame = spark.createDataFrame(rowRdd)
 
@@ -22,7 +45,16 @@ def process(rdd):
         wordCountsDataFrame = spark.sql("select word, count(*) as total from words group by word order by 2 desc")       
         # pd_df=wordCountsDataFrame.toPandas()
         wordCountsDataFrame.show()
+        l = wordCountsDataFrame.select("word","total").rdd.flatMap(lambda x: x).collect()
+        print(l)
 
+        #push this to kafka
+        for word, count in l:
+                producer.send(word, count)
+                # print(i)
+
+        #list_of_processed_events = wordsDataFrame.collect()
+        # producer.send('output_event', value = str(list_of_processed_events))
         # # df = pd_df.toPandas()
         # df.pprint()
         # try:
@@ -35,6 +67,9 @@ def process(rdd):
         # except:
         #     print("34 failed")
 
+#processing each micro batch
+def process_events(event):
+    return (event[0], Counter(event[1].split(" ")).most_common(3))
 
 
 ssc = StreamingContext(sc,10)
@@ -43,6 +78,11 @@ lines=socket_stream.window(10)
 df=lines.flatMap(lambda x:x.split(" ")).filter(lambda x:x.startswith("#")).filter(lambda x: x in ['#RCB','#CSK','#MI','#SRH'])
 # countdf = df.countByValue()
 # countdf.pprint()
+
+kafkaStream = KafkaUtils.createStream(ssc, 'localhost:2181', 'test-consumer-group', {'input_event':1})
+lines = kafkaStream.map(lambda x : process_events(x))
+
+
 df.foreachRDD(process)
 ssc.start()             
 ssc.awaitTermination()  
--- mongo_client.py
+++ mongo_client.py
@@ -4,11 +4,9 @@ from pymongo import MongoClient
 import json
 
 HASHTAG_LIST = [
-    "#RCB",
-    "#CSK",
-    "#MI",
-    "#IPL",
-    "#ELONMUSK"
+    "RCB",
+    "rcb",
+    "CB"
 ]
 
 def main():
@@ -20,37 +18,44 @@ def main():
     except:  
         print("Could not connect to MongoDB")
         return    
-    for hashtag in HASHTAG_LIST:
-        # connect kafka consumer to desired kafka topic	
-        consumer = KafkaConsumer(hashtag, bootstrap_servers=['localhost:9092'])
+    consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'])
+    consumer.subscribe(["RCB","CSK","MI","SRH","ELONMUSK"])
+    # consumer.subscribe("CB")
+    # consumer.subscribe("CSK")
+    # consumer.subscribe("MI")
+    # consumer.subscribe("SRH")
+    # consumer.subscribe("ElonMusk")
 
+    print("hello")
 
         # Parse received data from Kafka
-        for msg in consumer:
-            record = json.loads(msg.value)
-            name = record['name']
-            counts = int(record["count"]) 
-            timestamp = record["timestamp"]       
-            # Create dictionary and ingest data into MongoDB
-            try:
-                toBeInserted = {
-                    "name" : name,
-                    "count": counts,
-                    "timestamp": timestamp
-                }
-                id = db.hashtags.insert_one(toBeInserted)
-                print("Data inserted with record ids", id)
-            except:
-                print("Could not insert into MongoDB")
+    for msg in consumer:
+        print(msg)
+        #record = json.loads(msg.value)
+        name = msg.topic
+        counts = int(msg.value)      
+        # Create dictionary and ingest data into MongoDB
+        try:
+            toBeInserted = {
+                "name" : name,
+                "count": counts
+            }
+            id = db.hashtags.insert_one(toBeInserted)
+            print("Data inserted with record ids", id)
+        except:
+            print("Could not insert into MongoDB")
+    #consumer.close()
 
 
-client = MongoClient('localhost',27017)
-db = client.temp_data
-print("Connected successfully!")
-toBeInserted = {
-    "name" : "ELONMUSK",
-    "count": 69,
-    "timestamp": "2019-01-01"
-}
-id = db.hashtags.insert_one(toBeInserted)
-print("Data inserted with record ids", id)
+# client = MongoClient('localhost',27017)
+# db = client.temp_data
+# print("Connected successfully!")
+# toBeInserted = {
+#     "name" : "ELONMUSK",
+#     "count": 69,
+#     "timestamp": "2019-01-01"
+# }
+# id = db.hashtags.insert_one(toBeInserted)
+# print("Data inserted with record ids", id)
+
+main()
